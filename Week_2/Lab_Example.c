4af6638f4b556cb29046f8ba540d38fab0580d34
cassLaffan <poisonberryvines@gmail.com>
2024-09-11 15:03:31 -0400

    Adding this week's examples

--- Week_2/Lab_Example.c
+++ Week_2/Lab_Example.c
@@ -6,3 +6,26 @@
 * 2) What year they were born
 * 3) How many years until they can retire!
 */
+
+// Name: Cassandra
+// Student Number: 500965668
+
+#include <stdio.h>
+
+int main(){
+    int age, ageInMonths, birthYear, retirement;
+
+    printf("Enter your age: ");
+    scanf("%d", &age);
+
+    // Calculations
+    ageInMonths = age * 12;
+    birthYear = 2024 - age;
+    retirement = 65 - age; // assume 65 is the retirement year
+ 
+    printf("Your age, %d, in months is: %d\n", age, ageInMonths);
+    printf("Your birth year is (likely): %d\n", birthYear);
+    printf("You can probably retire in %d years!\n", retirement);
+
+    return 0;
+}
