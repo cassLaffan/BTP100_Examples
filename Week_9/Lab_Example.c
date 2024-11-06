aba83c0ad53275f927dc6e011728c990722f816c
cassLaffan <poisonberryvines@gmail.com>
2024-11-06 15:41:43 -0500

    Doesn't work on my machine

--- Week_9/Lab_Example.c
+++ Week_9/Lab_Example.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 /*
 * In this lab, we will be practicing more with functions and 
 * pointers. The first function will take two arguments:
@@ -17,7 +18,47 @@
 * Hint:     scanf("%[^\n]",arr);getchar();
 */
 
+int lengthMatch(char* str, int len){
+    int count = 0;
+
+    // If our string exists at all
+    if(str){
+        // If it doesn't, this while loop would give us an index error
+        while(str[count] != '\0'){
+            count++;
+        }
+    }
+    // Will return 1 or 0 depending on the truthfulness of the answer here
+    return count == len;
+}
+
+void overwriteString(char* str){
+    if(!str){
+        printf("This string does not exist!\n");
+        // Allocating new memory for str in case it's a NULL
+        str = (char*)realloc(str, sizeof(char) * 100);
+    }
+    printf("Enter a new string: ");
+    scanf("%[^\n]", str);
+    getchar();
+}
+
+
 int main(){
+    char name[15] = "Cassandra\0";
+
+    int result = lengthMatch(name, 9);
+
+    printf("The result of our first function is: %d\n", result);
+
+    overwriteString(name);
+
+    printf("Our new string is: %s\n", name);
+
+    char* newChar = NULL;
+
+    overwriteString(newChar);
+    printf("%s\n", newChar);
 
     return 0;
 }
