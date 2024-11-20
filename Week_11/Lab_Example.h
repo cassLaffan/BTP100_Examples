757d5da9fd2c7062f771be7895eb7aa6b4d1aab1
cassLaffan <poisonberryvines@gmail.com>
2024-11-20 12:30:40 -0500

    Adding the bones for this week's example

---
+++ Week_11/Lab_Example.h
@@ -0,0 +1,16 @@
+/*
+ * This week, we will be working more with header files. 
+ * We will create a Node struct, which will take the form
+ * of this:
+ * 
+ * Node -> Next Node
+ * 
+ * That next node can point, furthermore, to another
+ * node and so on. We will then write some functions for this
+ * "data structure", such as a search function and an insert!
+ * 
+ * Finally, we will write a function that reads the integers from
+ * ints.txt and creates a node -> node -> node data structure
+ * with the numbers from it.
+ * 
+*/
